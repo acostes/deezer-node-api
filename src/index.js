66c2ad4df1ea8f7e084890f7741c88fcf69e68e5
acostes <arnaud.costes@gmail.com>
2016-10-16 11:20:03 +0200

    fix node version test

--- src/index.js
+++ src/index.js
@@ -2,20 +2,18 @@
 
 var rp = require('request-promise');
 
-class Deezer {
-    constructor() {
-        this.apiUrl = 'https://api.deezer.com/';
-    }
+function Deezer() {
+    this.apiUrl = 'https://api.deezer.com/';
+}
 
-    getAlbum(id) {
-        var url = 'album/' + id;
-        return rp({url: this.apiUrl + url, json:true});
-    }
+Deezer.prototype.getAlbum = function(id) {
+    var url = 'album/' + id;
+    return rp({url: this.apiUrl + url, json:true});
+}
 
-    getArtist(id) {
-        var url = 'artist/' + id;
-        return rp({url: this.apiUrl + url, json:true});
-    }
+Deezer.prototype.getArtist = function(id) {
+    var url = 'artist/' + id;
+    return rp({url: this.apiUrl + url, json:true});
 }
 
 module.exports = Deezer;
